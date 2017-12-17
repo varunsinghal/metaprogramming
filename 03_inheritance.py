f97c7eaed03b926156e6b0eadd39d8df45ec8f9a
Varun Singhal <varunsinghal15@gmail.com>
2017-12-17 16:39:09 +0530

    Create 03_inheritance.py

---
+++ 03_inheritance.py
@@ -0,0 +1,11 @@
+# Inheritance
+# Take method/property from base class and modify it in child class.
+
+class Base:
+    def spam(self):
+        pass
+
+
+class Foo(Base):
+    def spam(self):
+        pass
